f866191f44e2555d878fee3c537bd5e26550b064
Pat <p171982@gmail.com>
2020-04-25 20:25:21 +0100

    read one record from DB

--- models/Post.php
+++ models/Post.php
@@ -46,5 +46,31 @@ class Post {
          return $stmt;
     }
 
+    //get SINGLE POST 
+    public function read_single(){
+        //query to get post from DB
+        $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.createad_at
+                        FROM ' . $this->table . ' p
+                        LEFT JOIN
+                            categories c ON p.category_id = c.id
+                        WHERE
+                            p.id = ?
+                        LIMIT 0,1';
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
+        //bind ID to placeholder "?"
+        $stmt->bindParam(1,$this->id);
+        //execute query
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //set properties to those from $row
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+
+    }
 
 }
